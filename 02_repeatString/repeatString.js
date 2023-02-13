58df588b74126225952755095d1f85b5fb58b66e
Tim Tognazzini <ttognazzini@Fabricuts-MacBook-Pro.local>
2023-02-13 09:51:13 -0600

    Complete excercise1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string,repeatCount) {
+
+	if (repeatCount < 0) return 'ERROR';
+
+	let returnString = '';
+
+	for (let i = 0; i < repeatCount; i++) {
+		returnString += string;
+	}
+
+	return returnString;
 
 };
 
