4f4514d1da7100ce120f5b005c93dd59321b2aec
Tim Tognazzini <ttognazzini@Fabricuts-MacBook-Pro.local>
2023-02-15 10:14:20 -0600

    Complete excercises 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
 
+	// validate incoming parameters
+	if (typeof startNumber != "number" ) {return "ERROR"}
+	if (typeof endNumber != "number" ) {return "ERROR"}
+	if (startNumber < 0 ) {return "ERROR"}
+	if (endNumber < 0 ) {return "ERROR"}
+
+	// if the start number is larger, switch them
+	if (startNumber > endNumber) {let save = startNumber;startNumber=endNumber;endNumber=save}
+
+	let sum = 0;
+	for (let i = startNumber; i <= endNumber; i++) {
+		sum += i;
+	}
+	return sum;
 };
 
 // Do not edit below this line
