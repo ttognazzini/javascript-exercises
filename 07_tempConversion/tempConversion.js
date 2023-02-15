9ac6369877c888bd6beeade54e49a80eb0c91752
Tim Tognazzini <ttognazzini@Fabricuts-MacBook-Pro.local>
2023-02-15 14:48:09 -0600

    Complete excercises 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  return round((f -32 ) * (5/9),1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return round((c * (9/5) + 32),1)
 };
+
+const round = function(number, percision = 0) {
+  return Math.round((number + Number.EPSILON) * 10**percision) / 10**percision
+}
 
 // Do not edit below this line
 module.exports = {
