17676856c131d618f7fd921987747458a747dcd1
Tim Tognazzini <ttognazzini@Fabricuts-MacBook-Pro.local>
2023-02-15 09:56:40 -0600

    Complete excercises 3 and 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray,...elementsToRemove) {
+
+	elementsToRemove.forEach(function (elementToRemove) {
+		for (let i = 0; i < inputArray.length;i++) {
+			if (inputArray[i] === elementToRemove) {inputArray.splice(i,1)}
+		}
+	});
+
+	return inputArray;
 
 };
 
