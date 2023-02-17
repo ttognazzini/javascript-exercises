d118515687533d693fae809012c8caf483c3f953
Tim Tognazzini <ttognazzini@Fabricuts-MacBook-Pro.local>
2023-02-17 07:46:36 -0600

    complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+  let stripped = input.replace(/[\p{P}$+<=>^`|~]/gu, '').replace(/ /g,'').toLowerCase();
+  return (stripped == stripped.split("").reverse().join("")) ? true : false;
 };
 
 // Do not edit below this line
