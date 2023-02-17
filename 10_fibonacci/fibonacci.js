30fd316527e732bac4f1246f33bbd60cc4e85ff0
Tim Tognazzini <ttognazzini@Fabricuts-MacBook-Pro.local>
2023-02-17 08:04:56 -0600

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+	if (number < 1)  {return "OOPS"}
+	if (number == 1 || number == 2) return 1;
+	let fibonacci = [1,1];
+	for (let i = 1; i < number; i++) {
+		fibonacci.push(fibonacci[i-1] + fibonacci[i]);
+	}
+	return fibonacci[number-1];
 };
 
 // Do not edit below this line
