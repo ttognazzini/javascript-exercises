bc1f1aa3dc3f8210050b1f174cc63a7c491f3904
Tim Tognazzini <ttognazzini@Fabricuts-MacBook-Pro.local>
2023-02-17 08:12:19 -0600

    complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,5 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+	return array.map((value, index, array) => {return array[index].title})
 };
 
 // Do not edit below this line
