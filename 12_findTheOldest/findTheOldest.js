49b1042729c0280f56fe2f48e6102af961ad2b30
Tim Tognazzini <ttognazzini@Fabricuts-MacBook-Pro.local>
2023-02-17 08:38:03 -0600

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+	return array.reduce((max, value) => 
+		(!max) ? value : 
+		(!max.yearOfDeath) ? (new Date().getFullYear() - max.yearOfBirth > value.yearOfDeath - value.yearOfBirth) ? max : value :
+		(max.yearOfDeath - max.yearOfBirth > value.yearOfDeath - value.yearOfBirth) ? max : value
+	);
 };
 
 // Do not edit below this line
