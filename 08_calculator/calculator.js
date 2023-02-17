b11625b3a83b90d19aaa8d1a705e77cfa3aea2d4
Tim Tognazzini <ttognazzini@Fabricuts-MacBook-Pro.local>
2023-02-17 07:27:32 -0600

    complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return (a === undefined || a.length ==0) ? 0 : a.reduce( f = (total,value, index, array) => total + value);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+	return (a === undefined || a.length ==0) ? 0 : a.reduce( f = (total,value, index, array) => total * value);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0 || a === 1) {return 1}  
+	else {
+	  	for (let i = a-1; i >= 1; i--) {a *= i};
+	    return a;
+	  }
 };
 
 // Do not edit below this line
